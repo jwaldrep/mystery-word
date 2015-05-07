36b3a386ece43974af4d17fa2ef0e4ba850b3340
John Waldrep <johnwaldrep@gmail.com>
2015-05-07 12:54:33 -0400

    Add prototype functions

---
+++ mystery_word.py
@@ -0,0 +1,19 @@
+
+
+def easy_words(word_list):
+    pass
+
+def medium_words(word_list):
+    pass
+
+def hard_words(word_list):
+    pass
+
+def random_word(word_list):
+    pass
+
+def display_word(word, word_list):
+    pass
+
+def is_word_complete(word, word_list):
+    pass
