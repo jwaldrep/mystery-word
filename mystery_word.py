67ddfb4a086982e1ba3886e780a315ddf1a8035f
John Waldrep <johnwaldrep@gmail.com>
2015-05-07 13:17:42 -0400

    Implement the easy functions to past 5/6 tests

--- mystery_word.py
+++ mystery_word.py
@@ -1,19 +1,23 @@
+import random
 
 
 def easy_words(word_list):
-    pass
+    return [word for word in word_list if 4 <= len(word) <= 6]
 
 def medium_words(word_list):
-    pass
+    return [word for word in word_list if 6 <= len(word) <= 8]
 
 def hard_words(word_list):
-    pass
+    return [word for word in word_list if 8 <= len(word)]
 
 def random_word(word_list):
-    pass
+    return random.choice(word_list)
 
 def display_word(word, word_list):
     pass
 
-def is_word_complete(word, word_list):
-    pass
+def is_word_complete(word, letter_list):
+    for letter in word:
+        if letter not in letter_list:
+            return False
+    return True
